d936dbf33a0227a627ed33ed71ab1f6f73c360e2
boyzcomzretr <ballin.alex@ya.ru>
2026-03-31 15:22:49 +0300

    Initial Commit

---
+++ FinancesF/FinancesFApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancesFApp.swift
+//  FinancesF
+//
+//  Created by Александр Малахов on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct FinancesFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
